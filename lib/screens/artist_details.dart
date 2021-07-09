31bd9028c98e23d72638db07572f4895f2d4809b
Stefan Schätz <stefan.schaetz@codecentric.de>
2021-07-09 19:45:23 +0200

    Display artist name on artist details page

--- lib/screens/artist_details.dart
+++ lib/screens/artist_details.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import 'package:moodtag/models/artist.dart';
+import 'package:moodtag/models/library.dart';
 
 class ArtistDetailsPage extends Page {
   final String title;
@@ -21,6 +23,8 @@ class ArtistDetailsScreen extends StatelessWidget {
   final String title;
   final Artist artist;
 
+  static const artistNameStyle = TextStyle(fontWeight: FontWeight.bold, fontSize: 28);
+
   ArtistDetailsScreen({
     @required this.title,
     @required this.artist,
@@ -32,15 +36,31 @@ class ArtistDetailsScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text(title),
       ),
-      body: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Wrap(
-            children: [
-              Chip(label: Text('Rock')),
-              SizedBox(width: 8),
-              Chip(label: Text('70s')),
-            ],
-          )),
+      body: Consumer<Library>(
+        builder: (context, library, child) {
+          return Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: EdgeInsets.only(bottom: 12.0),
+                  child: Text(artist.name, style: artistNameStyle),
+                ),
+                Wrap(
+                  spacing: 1.0,
+                  runSpacing: 2.0,
+                  children: [
+                    Chip(label: Text('Rock')),
+                    SizedBox(width: 8),
+                    Chip(label: Text('70s')),
+                  ],
+                )
+              ]
+            ),
+          );
+        }
+      ),
     );
   }
 }
