d98ac6410fbafc61de6e597edad0bc33d29531cd
Stefan Schätz <stefan.schaetz@codecentric.de>
2021-10-15 18:23:30 +0200

    Implement data persistence using Drift (WIP)

---
+++ lib/database/moodtag_db.dart
@@ -0,0 +1,56 @@
+import 'dart:io';
+
+import 'package:drift/drift.dart';
+import 'package:drift/native.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:path/path.dart' as path;
+
+part 'moodtag_db.g.dart';
+
+
+class Artists extends Table {
+  IntColumn get id => integer().autoIncrement()();
+  TextColumn get name => text().withLength(max: 255)();
+}
+
+class Tags extends Table {
+  IntColumn get id => integer().autoIncrement()();
+  TextColumn get name => text().withLength(max: 255)();
+}
+
+
+@DriftDatabase(tables: [Artists, Tags])
+class MoodtagDB extends _$MoodtagDB {
+
+  MoodtagDB() : super(_openConnection());
+
+  @override
+  int get schemaVersion => 1;
+
+  Future<List<Artist>> get allArtists => select(artists).get();
+  Future<List<Tag>> get allTags => select(tags).get();
+
+  Future<int> addArtist(ArtistsCompanion artist) {
+    return into(artists).insert(artist);
+  }
+  Future<int> addTag(TagsCompanion tag) {
+    return into(tags).insert(tag);
+  }
+
+  Future<int> deleteArtist(int artistId) {
+    return (delete(artists)..where((a) => a.id.equals(artistId))).go();
+  }
+  Future<int> deleteTags(int tagId) {
+    return (delete(tags)..where((t) => t.id.equals(tagId))).go();
+  }
+
+
+}
+
+LazyDatabase _openConnection() {
+  return LazyDatabase(() async {
+      final dbFolder = await getApplicationDocumentsDirectory();
+      final file = File(path.join(dbFolder.path, 'db.sqlite'));
+      return NativeDatabase(file);
+  });
+}
