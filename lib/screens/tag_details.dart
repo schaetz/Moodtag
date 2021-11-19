9a165482db189f51162d5cbeab8947243db568c5
Stefan Schätz <stefan.schaetz@codecentric.de>
2021-11-19 11:16:00 +0100

    Adjust tag details page to BLoC pattern

--- lib/screens/tag_details.dart
+++ lib/screens/tag_details.dart
@@ -2,11 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 import 'package:moodtag/components/mt_app_bar.dart';
+import 'package:moodtag/database/moodtag_bloc.dart';
+import 'package:moodtag/database/moodtag_db.dart';
 import 'package:moodtag/dialogs/add_entity_dialog.dart';
 import 'package:moodtag/dialogs/remove_tag_from_artist_dialog.dart';
-import 'package:moodtag/models/artist.dart';
-import 'package:moodtag/models/library.dart';
-import 'package:moodtag/models/tag.dart';
 import 'package:moodtag/navigation/routes.dart';
 
 class TagDetailsScreen extends StatelessWidget {
@@ -21,36 +20,55 @@ class TagDetailsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final bloc = Provider.of<MoodtagBloc>(context, listen: false);
+
     return Scaffold(
       appBar: MtAppBar(context),
-      body: Consumer<Library>(
-        builder: (context, library, child) {
+      body: StreamBuilder<List<Artist>>(
+        stream: bloc.artistsWithTag(tag),
+        builder: (context, snapshot) {
+          List<Artist> artistsWithTag = snapshot.hasData ? snapshot.data : [];
+
           return Padding(
             padding: const EdgeInsets.all(16.0),
             child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Padding(
-                  padding: EdgeInsets.only(bottom: 12.0),
-                  child: Text(tag.name + ' (' + library.getArtistsWithTag(tag).length.toString() + ')', style: tagNameStyle),
-                ),
-                Expanded(
-                  child: ListView.separated(
-                    separatorBuilder: (context, _) => Divider(),
-                    padding: EdgeInsets.all(16.0),
-                    itemCount: library.getArtistsWithTag(tag).length,
-                    itemBuilder: (context, i) {
-                      return _buildArtistRow(context, library.getArtistsWithTag(tag)[i]);
-                    },
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: EdgeInsets.only(bottom: 12.0),
+                    child: Text(
+                      tag.name + ' (' + artistsWithTag.length.toString() + ')',
+                      style: tagNameStyle
+                    ),
                   ),
-                ),
-              ],
+                  Expanded(
+                    child: Builder(
+                      builder: (BuildContext context) {
+                        if (artistsWithTag.isEmpty) {
+                          return const Align(
+                            alignment: Alignment.center,
+                            child: Text('No artists with this tag', style: listEntryStyle),
+                          );
+                        }
+
+                        return ListView.separated(
+                          separatorBuilder: (context, _) => Divider(),
+                          padding: EdgeInsets.all(16.0),
+                          itemCount: artistsWithTag.length,
+                          itemBuilder: (context, i) {
+                            return _buildArtistRow(context, artistsWithTag[i]);
+                          },
+                        );
+                      }
+                    )
+                  ),
+                ]
             ),
           );
         },
       ),
       floatingActionButton: FloatingActionButton(
-        //onPressed: () => AddEntityDialog.openAddArtistDialog(context, preselectedTag: tag), TODO fix
+        onPressed: () => AddEntityDialog.openAddArtistDialog(context, preselectedTag: tag),
         child: const Icon(Icons.add),
         backgroundColor: Theme.of(context).accentColor,
       ),
@@ -64,7 +82,7 @@ class TagDetailsScreen extends StatelessWidget {
         style: listEntryStyle,
       ),
       onTap: () => Navigator.of(context).pushNamed(Routes.artistsDetails, arguments: artist),
-      //onLongPress: () => RemoveTagFromArtistDialog.openNew(context, tag, artist) TODO fix
+      onLongPress: () => RemoveTagFromArtistDialog.openNew(context, tag, artist)
     );
   }
 
