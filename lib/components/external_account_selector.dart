4309b3659441a4562940644254664801db295aa4
Stefan Schätz <stefan.schaetz@codecentric.de>
2022-07-08 13:47:45 +0200

    Move Last.fm-specific logic from ExternalAccountSelector to LastFmImportScreen

--- lib/components/external_account_selector.dart
+++ lib/components/external_account_selector.dart
@@ -2,14 +2,16 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:moodtag/database/moodtag_bloc.dart';
-import 'package:moodtag/dialogs/add_lastfm_account_dialog.dart';
-import 'package:moodtag/utils/user_properties_index.dart';
-import 'package:provider/provider.dart';
 
 class ExternalAccountSelector extends StatefulWidget {
   final String serviceName;
+  final StreamController accountNameStreamController;
+  final Function onAddAccountClick;
+  final Function onRemoveAccountClick;
 
-  const ExternalAccountSelector({Key key, this.serviceName}) : super(key: key);
+  const ExternalAccountSelector(
+      {Key key, this.serviceName, this.accountNameStreamController, this.onAddAccountClick, this.onRemoveAccountClick})
+      : super(key: key);
 
   @override
   State<StatefulWidget> createState() => _ExternalAccountSelectorState();
@@ -17,28 +19,19 @@ class ExternalAccountSelector extends StatefulWidget {
 
 class _ExternalAccountSelectorState extends State<ExternalAccountSelector> {
   MoodtagBloc bloc;
-  StreamController<String> accountNameStreamController = StreamController<String>();
 
   @override
   void initState() {
     super.initState();
-    bloc = Provider.of<MoodtagBloc>(context, listen: false);
-    updateAccountNameFromDatabase();
   }
 
   @override
   Widget build(BuildContext context) {
     return StreamBuilder<String>(
-        stream: accountNameStreamController.stream,
+        stream: widget.accountNameStreamController.stream,
         builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
           return _accountNameContainer(snapshot.data);
         });
-  }
-
-  void updateAccountNameFromDatabase() {
-    bloc
-        .getUserProperty(UserPropertiesIndex.USER_PROPERTY_LASTFM_ACCOUNT_NAME)
-        .then((value) => accountNameStreamController.add(value));
   }
 
   Widget _accountNameContainer(String accountName) {
@@ -49,7 +42,7 @@ class _ExternalAccountSelectorState extends State<ExternalAccountSelector> {
       margin: const EdgeInsets.all(32.0),
       child: Column(children: [
         accountName == null
-            ? Text('No associated ${this.widget.serviceName} account', style: TextStyle(fontStyle: FontStyle.italic))
+            ? Text('No associated ${widget.serviceName} account', style: TextStyle(fontStyle: FontStyle.italic))
             : Text(accountName),
         SizedBox(height: 16),
         _accountChangeButton(accountName != null),
@@ -60,31 +53,14 @@ class _ExternalAccountSelectorState extends State<ExternalAccountSelector> {
   Widget _accountChangeButton(bool hasAccountName) {
     if (!hasAccountName) {
       return ElevatedButton(
-        onPressed: () => _openSetAccountNameDialog(),
-        child: Text('Add ${this.widget.serviceName} account'),
+        onPressed: () => widget.onAddAccountClick(),
+        child: Text('Add ${widget.serviceName} account'),
       );
     } else {
       return ElevatedButton(
-        onPressed: () => _setAccountName(null),
-        child: Text('Remove ${this.widget.serviceName} account'),
+        onPressed: () => widget.onRemoveAccountClick(),
+        child: Text('Remove ${widget.serviceName} account'),
       );
     }
-  }
-
-  void _openSetAccountNameDialog() async {
-    String newAccountName = await AddExternalAccountDialog(context, this.widget.serviceName).show();
-    if (newAccountName != null) {
-      _setAccountName(newAccountName);
-    }
-  }
-
-  void _setAccountName(String newAccountName) {
-    if (bloc == null) {
-      throw new Exception('${this.widget.serviceName} account name could not be set - BLoC object is not available');
-    }
-
-    bloc
-        .createOrUpdateUserProperty(UserPropertiesIndex.USER_PROPERTY_LASTFM_ACCOUNT_NAME, newAccountName)
-        .whenComplete(() => updateAccountNameFromDatabase());
   }
 }
--- lib/screens/lastfm_import_screen.dart
+++ lib/screens/lastfm_import_screen.dart
@@ -1,22 +1,69 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:moodtag/components/external_account_selector.dart';
 import 'package:moodtag/components/mt_app_bar.dart';
+import 'package:moodtag/database/moodtag_bloc.dart';
+import 'package:moodtag/dialogs/add_lastfm_account_dialog.dart';
+import 'package:moodtag/utils/user_properties_index.dart';
+import 'package:provider/provider.dart';
 
 class LastfmImportScreen extends StatefulWidget {
+  final String serviceName = 'Last.fm';
+
   @override
   State<StatefulWidget> createState() => _LastfmImportScreenState();
 }
 
 class _LastfmImportScreenState extends State<LastfmImportScreen> {
+  MoodtagBloc bloc;
+  StreamController<String> accountNameStreamController = StreamController<String>();
+
+  @override
+  void initState() {
+    super.initState();
+    bloc = Provider.of<MoodtagBloc>(context, listen: false);
+    _updateAccountNameFromDatabase();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: MtAppBar(context),
         body: Center(
           child: Column(
-            children: [ExternalAccountSelector(serviceName: "Last.fm")],
+            children: [
+              ExternalAccountSelector(
+                serviceName: widget.serviceName,
+                accountNameStreamController: accountNameStreamController,
+                onAddAccountClick: () => _openSetAccountNameDialog(),
+                onRemoveAccountClick: () => _setAccountName(null),
+              )
+            ],
           ),
         ));
+  }
+
+  void _updateAccountNameFromDatabase() {
+    bloc
+        .getUserProperty(UserPropertiesIndex.USER_PROPERTY_LASTFM_ACCOUNT_NAME)
+        .then((value) => accountNameStreamController.add(value));
+  }
+
+  void _openSetAccountNameDialog() async {
+    String newAccountName = await AddExternalAccountDialog(context, widget.serviceName).show();
+    if (newAccountName != null) {
+      _setAccountName(newAccountName);
+    }
+  }
+
+  void _setAccountName(String newAccountName) {
+    if (bloc == null) {
+      throw new Exception('${widget.serviceName} account name could not be set - BLoC object is not available');
+    }
+
+    bloc
+        .createOrUpdateUserProperty(UserPropertiesIndex.USER_PROPERTY_LASTFM_ACCOUNT_NAME, newAccountName)
+        .whenComplete(() => _updateAccountNameFromDatabase());
   }
 }
