cbbf45fb34e26ac4686b8572b6553c4cef5a5bdd
Stefan Schätz <stefan.schaetz@codecentric.de>
2023-11-29 15:43:16 +0100

    Implement button to show/hide tags on ArtistsListScreen

--- lib/screens/library/artists_list_screen_bottom_app_bar.dart
+++ lib/screens/library/artists_list_screen_bottom_app_bar.dart
@@ -1,28 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:moodtag/model/blocs/artists_list/artists_list_bloc.dart';
+import 'package:moodtag/model/blocs/artists_list/artists_list_state.dart';
+import 'package:moodtag/model/events/artist_events.dart';
 
 class ArtistsListScreenBottomAppBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return BottomAppBar(
-      child: Row(
-        children: <Widget>[
-          IconButton(
-            tooltip: 'Filter',
-            icon: const Icon(Icons.filter_list),
-            onPressed: () {},
-          ),
-          IconButton(
-            tooltip: 'Search',
-            icon: const Icon(Icons.search),
-            onPressed: () {},
-          ),
-          IconButton(
-            tooltip: 'Tags',
-            icon: const Icon(Icons.label),
-            onPressed: () {},
-          ),
-        ],
-      ),
-    );
+    final bloc = context.read<ArtistsListBloc>();
+    return BlocBuilder<ArtistsListBloc, ArtistsListState>(
+        builder: (context, state) => BottomAppBar(
+              child: Row(
+                children: <Widget>[
+                  IconButton(
+                    tooltip: 'Filter',
+                    icon: const Icon(Icons.filter_list),
+                    onPressed: () {},
+                  ),
+                  IconButton(
+                    tooltip: 'Search',
+                    icon: const Icon(Icons.search),
+                    onPressed: () {},
+                  ),
+                  IconButton(
+                    tooltip: state.displayTagSubtitles ? 'Hide Tags' : 'Show Tags',
+                    icon: state.displayTagSubtitles ? const Icon(Icons.label_off) : const Icon(Icons.label),
+                    onPressed: () => bloc.add(ToggleTagSubtitles()),
+                  ),
+                ],
+              ),
+            ));
   }
 }
